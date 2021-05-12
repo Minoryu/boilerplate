ede2259926095387e694a0ca879506a339f62072
minoryu <>
2021-05-12 14:56:50 +0900

    build util: {.hostname } in bash

---
+++ kitting/pi4-raspbian/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+source ../tools/utils/comments.sh
+
+comments "${BASH_SOURCE[0]##*/}" "let's go"
+
+cd ../tools
+read hostname
+if [ $hostname ]; then
+    sudo ./set-hostname.sh $hostname
+else
+    comments "ERROR" "skip to change hostname."
+fi
---
+++ kitting/tools/set-hostname.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+source ../tools/utils/comments.sh
+
+comments "${BASH_SOURCE[0]##*/}" "let's go"
+
+sed '$d' /etc/hostname
+sed -e '127.0.1.1' /etc/hosts
+
+echo $1 >> /etc/hostname
+echo "127.0.1.1		$1" >> /etc/hosts
