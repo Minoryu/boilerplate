cfe89c151c7b22634cd54e87f337a2e367b94d87
minoryu <>
2021-05-14 15:00:55 +0900

    build: util change the hostname

--- kitting/tools/set-hostname.sh
+++ kitting/tools/set-hostname.sh
@@ -3,8 +3,11 @@ source ../tools/utils/comments.sh
 
 comments "${BASH_SOURCE[0]##*/}" "let's go"
 
-sed '$d' /etc/hostname
-sed '$d' /etc/hosts
+sudo hostnamectl set-hostname $1
 
-echo $1 >> /etc/hostname
+comments "do below this commands as root user."
+cat <<EOD
+sed -i -e '\$d' /etc/hosts
 echo "127.0.1.1		$1" >> /etc/hosts
+
+EOD
