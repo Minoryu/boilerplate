128c387f75fe743fae72620de8a2c4b62bb46a33
minoryu <>
2021-05-14 15:26:28 +0900

    save point 20200514

--- kitting/pi4-raspbian/entrypoint.sh
+++ kitting/pi4-raspbian/entrypoint.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 source ../tools/utils/comments.sh
+source ../tools/utils/init.vimrc.sh
 
 comments "${BASH_SOURCE[0]##*/}" "let's go"
 
+
 cd ../tools
-read -p "input hostname > "hostname
+
+# change hostname
+read -p "input hostname > " hostname
 if [ $hostname ]; then
     sudo ./set-hostname.sh $hostname
-else
-    comments "ERROR" "skip to change hostname."
+    comments "hostname is changed" "$hostname"
 fi
+
+# enable locale en_US.UTF-8
+LANGUAGE=en_US.UTF-8
+sudo ./utils/toggle.comment-out.sh $LANGUAGE /etc/locale.gen
+sudo locale-gen $LANGUAGE
+sudo update-locale LANG=en_US.UTF-8
+
+comments "check locale" "$(locale -a)"
+
+sudo set_locale
+set_locale
+
+sudo set_initial_vim
+set_initial_vim
